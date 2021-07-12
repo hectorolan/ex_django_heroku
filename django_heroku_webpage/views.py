347c278bc89d9b1255b5fae87fc8fbeffd176ef0
Hector Olan <olanhector@gmail.com>
2021-07-11 21:03:56 -0400

    update application

--- django_heroku_webpage/views.py
+++ django_heroku_webpage/views.py
@@ -1,3 +1,76 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
+def index(request):
+    return render(request, 'django_heroku_webpage/index.html')
+
+@login_required
+def topics(request):
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'django_heroku_webpage/topics.html', context)
+
+@login_required
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if topic.owner != request.user:
+        raise Http404
+
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'django_heroku_webpage/topic.html', context)
+
+@login_required
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
+            form.save()
+            return redirect('django_heroku_webpage:topics')
+
+    context = {'form': form}
+    return render(request, 'django_heroku_webpage/new_topic.html', context)
+
+@login_required
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('django_heroku_webpage:topic', topic_id=topic_id)
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'django_heroku_webpage/new_entry.html', context)
+
+@login_required
+def edit_entry(request, entry_id):
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
+
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instace=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('django_heroku_webpage:topic', topic_id=topic.id)
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'django_heroku_webpage/edit_entry.html', context)
